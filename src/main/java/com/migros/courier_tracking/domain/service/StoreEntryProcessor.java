8d9c1817a9fcb75e08f913ab14d88fc982592b4f
Murat <murathanakbyk@gmail.com>
2026-02-24 11:46:47 +0300

    refactor: simplify store entry logic and remove transactional management - Removed @Transactional to avoid self-invocation issues and database connection overhead.

--- src/main/java/com/migros/courier_tracking/domain/service/StoreEntryProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.migros.courier_tracking.domain.service;
-
-import com.migros.courier_tracking.domain.model.Location;
-import com.migros.courier_tracking.domain.model.Store;
-
-public interface StoreEntryProcessor {
-    void processStoreEntry(Location location, Store store);
-}
