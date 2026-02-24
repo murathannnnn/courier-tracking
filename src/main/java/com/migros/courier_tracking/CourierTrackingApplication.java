40d2e469cad92edb429629c606e4b0b83d4957fd
Murat <murathanakbyk@gmail.com>
2026-02-24 11:08:42 +0300

    test commit

--- src/main/java/com/migros/courier_tracking/CourierTrackingApplication.java
+++ src/main/java/com/migros/courier_tracking/CourierTrackingApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class CourierTrackingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CourierTrackingApplication.class, args);
 	}
